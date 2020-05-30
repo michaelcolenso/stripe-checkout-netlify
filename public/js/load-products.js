329d2bb8dc5f3cc30ec854a4e980eef554cd46ab
Michael Colenso <github@michaelcolenso.com>
2020-05-29 23:50:01 -0700

    feature: we have a working store.

--- public/js/load-products.js
+++ public/js/load-products.js
@@ -1,3 +1,6 @@
+import { handleFormSubmission } from './stripe-purchase.js';
+
+
 function createProductFromTemplate(item) {
        const template = document.querySelector('#product');
        const product = template.content.cloneNode(true);
@@ -13,6 +16,9 @@ function createProductFromTemplate(item) {
        const img = product.querySelector('img');
        img.src = item.image;
        img.alt = item.name;
+
+       const form = product.querySelector('form');
+       form.addEventListener('submit', handleFormSubmission);
     
        return product;
      }
